46a35e1cd5c98e355b75c837b1bfd22730fa640a
Henrique Gustavo <henriqueoliveira310@gmail.com>
2018-01-02 13:42:04 -0200

    Ajuste das paginas de detalhamento

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
